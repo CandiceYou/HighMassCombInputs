136e4272340be4b63a668b186b084a0636236d66
Can You <cyou5@jhu.edu>
2017-02-10 22:29:27 +0100

    comment out spin-2

--- scripts/run2l2q_Templ.sh
+++ scripts/run2l2q_Templ.sh
@@ -1,19 +1,23 @@
 #check that root trees exist after efficiency step, and smoothing is done
+cp ../scripts/checkTempl_1Dslice_2Dsys.c .
+cp ../scripts/normTempl.c .
+cp ../scripts/
 
 if [ ! -d "2l2q2DTemplate" ]; then
   mkdir -p 2l2q2DTemplate/spin0/1DSlices/
-  mkdir -p 2l2q2DTemplate/spin2/1DSlices/
+#  mkdir -p 2l2q2DTemplate/spin2/1DSlices/
 fi
 
 #smoothing
-cd /afs/cern.ch/work/c/cayou/HighMass_RunII/CMSSW_7_6_3/src/ZZAnalysis/AnalysisStep/test/CombInputs/TemplateBuilder/
+cp ../smConfig/*.json ../../TemplateBuilder/run 
+cd ../../TemplateBuilder/
 eval `scramv1 runtime -sh` 
 ./buildTemplate.exe run/2l2q_2Dtempl_spin0.json
-./buildTemplate.exe run/2l2q_2Dtempl_spin2.json
 ./buildTemplate.exe run/2l2q_2Dtempl_spin0_up.json
-./buildTemplate.exe run/2l2q_2Dtempl_spin2_up.json
 ./buildTemplate.exe run/2l2q_2Dtempl_spin0_dn.json
-./buildTemplate.exe run/2l2q_2Dtempl_spin2_dn.json
+#./buildTemplate.exe run/2l2q_2Dtempl_spin2.json
+#./buildTemplate.exe run/2l2q_2Dtempl_spin2_up.json
+#./buildTemplate.exe run/2l2q_2Dtempl_spin2_dn.json
 cd -
 
 #normalize
@@ -22,9 +26,10 @@ root -l -n -b -q normTempl.c\(0,\"nominal\"\)
 root -l -n -b -q normTempl.c\(0,\"JECup\"\) 
 root -l -n -b -q normTempl.c\(0,\"JECdn\"\) 
 # spin2
-root -l -n -b -q normTempl.c\(2,\"nominal\"\)
-root -l -n -b -q normTempl.c\(2,\"JECup\"\) 
-root -l -n -b -q normTempl.c\(2,\"JECdn\"\)
+#root -l -n -b -q normTempl.c\(2,\"nominal\"\)
+#root -l -n -b -q normTempl.c\(2,\"JECup\"\) 
+#root -l -n -b -q normTempl.c\(2,\"JECdn\"\)
 
-#check 1D slices
-root -l -b -q checkTempl_1Dslice_2Dsys.c
+#check 1D slices, make alternative templates
+root -l -b -q checkTempl_1Dslice_2Dsys.c \(0\)   #spin0
+#root -l -b -q checkTempl_1Dslice_2Dsys.c \(2\)  #spin2
